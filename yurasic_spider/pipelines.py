28f1473d28318b0f65c1ff1c792fc87b0f07e722
stoyanovd <stoyanovd@mail.ru>
2016-04-25 20:43:08 +0300

    init

---
+++ yurasic_spider/pipelines.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import os
+
+authors_file = '/home/dima/PycharmProjects/yurasic_spider/authors.txt'
+
+
+class YurasicSpiderPipeline(object):
+    def __init__(self):
+        self.cur_authors = set()
+
+    def process_item(self, item, spider):
+        a = item['author']
+        if a not in self.cur_authors:
+            self.cur_authors.add(a)
+            with open(authors_file, 'a') as f:
+                f.write(a + os.linesep)
+        return item
